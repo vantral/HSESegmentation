f2e4b5b4382d9c71e1b3c7617daf498b3c7da820
Tasia <tasia@Tasias-Air.Dlink>
2022-10-27 13:51:41 +0300

    Add search bar

--- simple_test_app/app.py
+++ simple_test_app/app.py
@@ -2,24 +2,32 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    return render_template('main.html')
+
+
+@app.route('/play')
+def play():
     return render_template('play.html')
 
-@app.route('/transducer', methods = ['GET'])
+
+@app.route('/search')
+def search():
+    text = request.args.get('test')
+    return text * 4
+
+
+@app.route('/transducer', methods=['GET'])
 def how_to():
         return render_template('transducer.html')
+
 
 @app.route('/corpus')
 def main_page():
     return render_template('tsacorpus.html')
 
-#
-#
-# @app.route('/search')
-# def search():
-#     text = request.args.get('test')
-#     return text * 4
 
 if __name__ == '__main__':
     app.run(debug=True)
